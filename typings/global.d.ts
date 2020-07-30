b7be1a1ebad3203687d28a70bf83d1cad3e906bc
jiankang.huang <huangjiankang@chinabr.net>
2020-07-30 15:32:17 +0800

    :arrow_up:升级ts配置支持”可选链操作符“ :zap: 优化全局描述文件

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -3,22 +3,22 @@ import { MysqlError } from 'mysql';
 import * as Koa from 'koa';
 import { AxiosInstance } from 'axios';
 import * as Log4js from 'log4js';
-interface LOG4METHODS {
-  app: Log4js.Logger;
-  http: Log4js.Logger;
-  error: Log4js.Logger;
-  [key: string]: Log4js.Logger;
-}
 
 declare global {
-  export class encryption {
+  interface LOG4METHODS {
+    app: Log4js.Logger;
+    http: Log4js.Logger;
+    error: Log4js.Logger;
+    [key: string]: Log4js.Logger;
+  }
+  class encryption {
     /**
      * @description hash加密
      * @param {string} content 加密对象
      * @param {string} type 加密类型
      * @return {string} 加密后的值
      */
-    public static hash(content: string, type: string): string;
+    static hash(content: string, type: string): string;
     /**
      * @description hmac加密
      * @param {string} content 加密对象
@@ -26,20 +26,20 @@ declare global {
      * @param {string} secretkey 加密秘钥
      * @return {string} 加密后的值
      */
-    public static hmac(content: string, type: string, secretkey: string): string;
+    static hmac(content: string, type: string, secretkey: string): string;
   }
 
-  export class DB {
+  class DB {
     /**
      * @description 数据库操作方法
      * @param {string} sql sql操作语句
      * @param {any[]} query sql查询值
      * @returns {*} Promise<{code: number; msg?: MysqlError | string; result?: any}>
      */
-    public static handle(sql: string, query: any[]): Promise<{ code: number; msg?: MysqlError | string; result?: any }>;
+    static handle(sql: string, query: any[]): Promise<{ code: number; msg?: MysqlError | string; result?: any }>;
   }
 
-  export class redisDb {
+  class redisDb {
     /**
      * @description 设置键值对
      * @param key 键
@@ -47,14 +47,14 @@ declare global {
      * @param expire 过期时间（单位：秒，可为空，为空则不过期）
      * @return 200
      */
-    public static set(key: string, value: any, expire?: number): Promise<number | Error>;
+    static set(key: string, value: any, expire?: number): Promise<number | Error>;
 
     /**
      * @description 获取对应value
      * @param key 键
      * @return Promise<string | Error | null>
      */
-    public static get(key: string): Promise<string | Error | null>;
+    static get(key: string): Promise<string | Error | null>;
 
     /**
      * @description 判断是否存在该key
@@ -62,7 +62,7 @@ declare global {
      * @return Promise<boolean>
      */
 
-    public static exits(key: string): Promise<boolean>;
+    static exits(key: string): Promise<boolean>;
 
     /**
      * @description 根据key值删除数据
@@ -70,7 +70,7 @@ declare global {
      * @return Promise<boolean>
      */
 
-    public static del(keys: string | string[]): Promise<boolean>;
+    static del(keys: string | string[]): Promise<boolean>;
   }
 
   /**
@@ -82,7 +82,7 @@ declare global {
    * @param {any} data
    * @param {string} msg
    */
-  export function response(ctx: Koa.Context, status: number, result?: { data: any }, msg?: string): void;
+  function response(ctx: Koa.Context, status: number, result?: { data: any }, msg?: string): void;
 
   /**
    * @description session处理
@@ -90,13 +90,13 @@ declare global {
    * @function get 获取session
    * @function delete 删除session
    */
-  export class Session {
+  class Session {
     public static set(ctx: Koa.Context, key: string, value: any, age: number): void;
     public static get(ctx: Koa.Context, key: string): string | null;
     public static delete(ctx: Koa.Context, key: string): void;
   }
 
-  export class JWT {
+  class JWT {
     /**
      * @description  token生成方法
      * @param {string | object | Buffer} value 加密对象
@@ -104,17 +104,17 @@ declare global {
      * @param {string} [expires='7 days'] 生成的token有效期
      * @return {string | boolean} 返回Promise对象(生成的token 失败返回false)
      */
-    public static generate(value: string | object | Buffer, secret?: string, expires?: string): Promise<string | boolean>;
+    static generate(value: string | object | Buffer, secret?: string, expires?: string): Promise<string | boolean>;
     /**
      * @description  验证token是否有效的方法
      * @param {string} token 需要验证的token
      * @param {string} [secret='^@q676V_8&2'] token加密秘钥
      * @return {object | string | boolean} 返回Promise对象(token解密成功的值,失败返回false)
      */
-    public static verify(token: string, secret?: string): Promise<object | string | boolean>;
+    static verify(token: string, secret?: string): Promise<object | string | boolean>;
   }
 
-  export function getParams(ctx: Koa.Context): { [key: string]: any };
+  function getParams(ctx: Koa.Context): { [key: string]: any };
 
   const $http: AxiosInstance;
 
