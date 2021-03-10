53d1ef6828d00d06cff8dbd013b78d3682fca160
jiankang.huang <huangjiankang@chinabr.net>
2021-03-10 11:54:31 +0800

    :bug: fix tslint check

--- src/controllers/wechat/wxbizDataCrypt/index.ts
+++ src/controllers/wechat/wxbizDataCrypt/index.ts
@@ -26,7 +26,7 @@ export default class WxBizDataCrypt {
       const decipher = createDecipheriv('aes-128-cbc', sessionKey, ivBuffer);
       // 设置自动 padding 为 true，删除填充补位
       decipher.setAutoPadding(true);
-      decoded = decipher.update(encryptedDataBuffer, 'binary', 'utf8');
+      decoded = decipher.update(encryptedDataBuffer, undefined, 'utf8');
       decoded += decipher.final('utf8');
       decoded = JSON.parse(decoded);
     } catch (err) {
@@ -40,3 +40,29 @@ export default class WxBizDataCrypt {
     return decoded;
   }
 }
+
+// const appId = 'wx4f4bc4dec97d474b';
+// const sessionKey = 'tiihtNczf5v6AKRyjwEUhQ==';
+// const encryptedData =
+//   'CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZM' +
+//   'QmRzooG2xrDcvSnxIMXFufNstNGTyaGS' +
+//   '9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+' +
+//   '3hVbJSRgv+4lGOETKUQz6OYStslQ142d' +
+//   'NCuabNPGBzlooOmB231qMM85d2/fV6Ch' +
+//   'evvXvQP8Hkue1poOFtnEtpyxVLW1zAo6' +
+//   '/1Xx1COxFvrc2d7UL/lmHInNlxuacJXw' +
+//   'u0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn' +
+//   '/Hz7saL8xz+W//FRAUid1OksQaQx4CMs' +
+//   '8LOddcQhULW4ucetDf96JcR3g0gfRK4P' +
+//   'C7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB' +
+//   '6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns' +
+//   '/8wR2SiRS7MNACwTyrGvt9ts8p12PKFd' +
+//   'lqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYV' +
+//   'oKlaRv85IfVunYzO0IKXsyl7JCUjCpoG' +
+//   '20f0a04COwfneQAGGwd5oa+T8yO5hzuy' +
+//   'Db/XcxxmK01EpqOyuxINew==';
+// const iv = 'r7BXXKkLb8qrSNn05n0qiA==';
+
+// const t = new WxBizDataCrypt(appId, sessionKey, encryptedData, iv);
+// const r = t.decryptData();
+// console.log(r);
