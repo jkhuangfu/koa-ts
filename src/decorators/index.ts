47fba70577ea57ccc974aa26a684de16af4c4a32
jiankang.huang <huangjiankang@chinabr.net>
2021-05-08 14:08:55 +0800

    :beers: 路由装饰器增加鉴权方法

--- src/decorators/index.ts
+++ src/decorators/index.ts
@@ -1,6 +1,6 @@
 import * as KoaRouter from 'koa-router';
 import * as Koa from 'koa';
-
+import jwtCheck from '@/middleware/checklogin';
 type Middleware = (ctx: Koa.Context, next: Koa.Next) => void;
 
 enum RequestMethod {
@@ -24,7 +24,6 @@ type OPTIONS = {
  * @param {string} url 请求路径
  * @param {Methods} method 请求方法 e.g: post
  * @param {OPTIONS} options 可选参数 log是否打印返回值日志 middleware 中间件
- * @returns
  */
 export function Request(url: string, method: Methods, options: OPTIONS = { log: true, middleware: [] }) {
   return (target: any, name: string, descriptor: PropertyDescriptor) => {
@@ -33,11 +32,22 @@ export function Request(url: string, method: Methods, options: OPTIONS = { log: 
     descriptor.value = (router: any) => {
       router[method](url, ...middlewareArray, async (ctx: Koa.Context, next: Koa.Next) => {
         await fn(ctx, next);
-        // 记录返回值
-        if (options.log) {
-          LOG4.http.trace(`请求连接:${ctx.path}, 传递参数:${JSON.stringify(getParams(ctx))},返回值:${JSON.stringify(ctx.body)}`);
-        }
+        // log记录
+        options.log && LOG4.http.trace(`请求连接:${ctx.path}, 传递参数:${JSON.stringify(getParams(ctx))},返回值:${JSON.stringify(ctx.body)}`);
       });
+    };
+  };
+}
+
+/**
+ * @description 鉴权中间件
+ */
+export function Validate() {
+  return (target: object, name: string, descriptor: PropertyDescriptor) => {
+    const fn = descriptor.value;
+    descriptor.value = async (router: any) => {
+      router.use(jwtCheck);
+      fn(router);
     };
   };
 }
@@ -45,16 +55,15 @@ export function Request(url: string, method: Methods, options: OPTIONS = { log: 
 /**
  * @export
  * @param {string} [prefix=''] 请求路径前缀 默认空
- * @returns {*}
  */
 export function Controller(prefix: string = ''): any {
   const router = new KoaRouter({ prefix });
   return (target: ClassDecorator) => {
-    const reqList = Object.getOwnPropertyDescriptors(target.prototype);
-    for (const v in reqList) {
+    const fnList = Object.getOwnPropertyDescriptors(target.prototype);
+    for (const v in fnList) {
       // 排除类的构造方法
       if (v !== 'constructor') {
-        const fn = reqList[v].value;
+        const fn = fnList[v].value;
         fn(router);
       }
     }
